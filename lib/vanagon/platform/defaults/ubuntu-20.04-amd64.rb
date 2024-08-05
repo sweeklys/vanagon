c2a0f9afcd5d495710f8d9f3bf5469da51b81ea5
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2024-08-05 12:56:56 +0000

    (ospuppet) fix internal artifactory hack

--- lib/vanagon/platform/defaults/ubuntu-20.04-amd64.rb
+++ lib/vanagon/platform/defaults/ubuntu-20.04-amd64.rb
@@ -4,9 +4,11 @@ platform "ubuntu-20.04-amd64" do |plat|
   plat.servicetype "systemd"
   plat.codename "focal"
 
-  # Temporary fix to add focal-updates.list repo file because it is missing from the vmpooler image
-  plat.provision_with "echo 'deb https://artifactory.delivery.puppetlabs.net/artifactory/ubuntu__remote focal-updates main restricted universe multiverse' > /etc/apt/sources.list.d/focal-updates.list;
-  echo 'deb-src https://artifactory.delivery.puppetlabs.net/artifactory/ubuntu__remote focal-updates main restricted universe multiverse' >> /etc/apt/sources.list.d/focal-updates.list"
+  if %w[y yes true].include? ENV.fetch('VANAGON_USE_MIRRORS', 'y').downcase
+    # Temporary fix to add focal-updates.list repo file because it is missing from the vmpooler image
+    plat.provision_with "echo 'deb https://artifactory.delivery.puppetlabs.net/artifactory/ubuntu__remote focal-updates main restricted universe multiverse' > /etc/apt/sources.list.d/focal-updates.list;
+    echo 'deb-src https://artifactory.delivery.puppetlabs.net/artifactory/ubuntu__remote focal-updates main restricted universe multiverse' >> /etc/apt/sources.list.d/focal-updates.list"
+  end
   
   packages = %w(build-essential devscripts make quilt pkg-config debhelper rsync fakeroot cmake)
 
