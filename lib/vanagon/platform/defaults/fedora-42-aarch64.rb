7cf5380c06b2d27443cb15aa698d84a38893eb1a
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-04-14 09:32:01 -0700

    Add Fedora 42 platforms

---
+++ lib/vanagon/platform/defaults/fedora-42-aarch64.rb
@@ -0,0 +1,39 @@
+platform 'fedora-42-aarch64' do |plat|
+    plat.servicedir '/usr/lib/systemd/system'
+    plat.defaultdir '/etc/sysconfig'
+    plat.servicetype 'systemd'
+    plat.dist 'fc42'
+  
+    packages = %w(
+      autoconf
+      automake
+      binutils
+      bzip2-devel
+      cmake
+      curl
+      gcc
+      gcc-c++
+      libselinux-devel
+      libsepol
+      libsepol-devel
+      make
+      perl-lib
+      perl-FindBin
+      pkgconfig
+      readline-devel
+      rpmdevtools
+      rsync
+      swig
+      systemtap-sdt-devel
+      systemtap-sdt-dtrace
+      systemd
+      which
+      zlib-devel
+    )
+    plat.provision_with("/usr/bin/dnf install -y --best --allowerasing #{packages.join(' ')}")
+  
+    plat.install_build_dependencies_with '/usr/bin/dnf install -y --best --allowerasing'
+    plat.vmpooler_template 'fedora-42-aarch64'
+    plat.docker_image "fedora:42"
+    plat.docker_arch 'linux/arm64'
+  end
